bbc749676482711c8d9a950948980a561cb0fe6e
ziggity <zachbeecher@gmail.com>
2017-08-30 20:21:45 -0700

    keyboard component - not fully tested need more internet sluething to find out how to get it to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { KeyboardComponent } from './keyboard/keyboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KeyboardComponent
   ],
   imports: [
     BrowserModule,
